ba51f959c81870f64700b6e37ee18b3dfcf59790
Gayanka <gayankadhrm@gmail.com>
2025-04-25 07:36:18 +0530

    Find GCD of two numbers in java

---
+++ 29 - Find GCD of two Numbers in Java/JavaTestClass.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class JavaTestClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int number1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int number2 = sc.nextInt();
+
+        while (number1 != number2) {
+            if (number1 > number2) {
+                number1 = number1 - number2;
+            } else {
+                number2 = number2 - number1;
+            }
+        }
+
+        System.out.println("GCD = " + number1);
+    }
+}
