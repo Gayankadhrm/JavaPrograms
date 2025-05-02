434bf82866c00f2d7c2fab83cfa1f6f56101db4c
Gayanka <gayankadhrm@gmail.com>
2025-05-02 08:12:09 +0530

    Convert miles to kilometers

---
+++ 36. Convert Miles to Kilometers/MilesToKm.java
@@ -0,0 +1,8 @@
+
+public class MilesToKm {
+    public static void main(String[] args) {
+        double miles = 6.21371;
+        double km = miles / 0.621371;
+        System.out.println("Kilometers: " + km);
+    }
+}
