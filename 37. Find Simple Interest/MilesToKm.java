cedab438d377267e3dffb5cce01fcd419b2230a3
Gayanka <gayankadhrm@gmail.com>
2025-05-03 06:33:03 +0530

    Find simple iinterest

---
+++ 37. Find Simple Interest/MilesToKm.java
@@ -0,0 +1,7 @@
+public class MilesToKm {
+    public static void main(String[] args) {
+        double miles = 6.21371;
+        double km = miles / 0.621371;
+        System.out.println("Kilometers: " + km);
+    }
+}
