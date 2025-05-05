e80571e3aa6d8c83e288b391b4deeb63cfba5887
Gayanka <gayankadhrm@gmail.com>
2025-05-05 13:41:56 +0530

    Find area of rectangle

---
+++ 39. Find Area of Rectangle/RectangleArea.java
@@ -0,0 +1,6 @@
+public class RectangleArea { 
+    public static void main(String[] args) { 
+        double length = 5, width = 3; 
+        System.out.println("Area: " + (length * width)); 
+    } 
+}
