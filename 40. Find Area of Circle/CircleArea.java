1ddc326f8204128d099d2d49828cc016e4f66157
Gayanka <gayankadhrm@gmail.com>
2025-05-06 06:20:58 +0530

    Find the area of circle

---
+++ 40. Find Area of Circle/CircleArea.java
@@ -0,0 +1,6 @@
+public class CircleArea { 
+    public static void main(String[] args) { 
+        double radius = 5; 
+        System.out.println("Area: " + (Math.PI * radius * radius)); 
+    } 
+} 
