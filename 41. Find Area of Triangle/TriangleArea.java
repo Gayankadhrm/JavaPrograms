2ffe119488a53f06a23ba4010df9d75b1a7ab253
Gayanka <gayankadhrm@gmail.com>
2025-05-07 06:03:30 +0530

    Fnd area of triangle

---
+++ 41. Find Area of Triangle/TriangleArea.java
@@ -0,0 +1,6 @@
+public class TriangleArea { 
+    public static void main(String[] args) { 
+        double base = 5, height = 3; 
+        System.out.println("Area: " + (0.5 * base * height)); 
+    } 
+}
