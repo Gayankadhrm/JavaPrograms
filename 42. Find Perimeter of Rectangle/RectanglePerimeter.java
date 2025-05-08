349c0b57c75ecd6f2db17e5912fde6dedfb62eab
Gayanka <gayankadhrm@gmail.com>
2025-05-08 07:51:04 +0530

    Find perimeter of rectangle

---
+++ 42. Find Perimeter of Rectangle/RectanglePerimeter.java
@@ -0,0 +1,6 @@
+public class RectanglePerimeter { 
+    public static void main(String[] args) { 
+        double length = 5, width = 3; 
+        System.out.println("Perimeter: " + (2 * (length + width))); 
+    } 
+} 
