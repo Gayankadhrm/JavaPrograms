1f95f0e97767064497914d985cb3fc6c1cfe18cf
Gayanka <gayankadhrm@gmail.com>
2025-05-09 07:45:01 +0530

    Find circumference of circle

---
+++ 43. Find Circumference of Circle/CircleCircumference.java
@@ -0,0 +1,6 @@
+public class CircleCircumference { 
+    public static void main(String[] args) { 
+        double radius = 5; 
+        System.out.println("Circumference: " + (2 * Math.PI * radius)); 
+    } 
+}
