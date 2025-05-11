dab4797201bbfbe3ac217b1160e87baecdd57393
Gayanka <gayankadhrm@gmail.com>
2025-05-11 18:12:39 +0530

    Find volume of cylinder

---
+++ 45. Find Volume of Cylinder/CylinderVolume.java
@@ -0,0 +1,6 @@
+public class CylinderVolume { 
+    public static void main(String[] args) { 
+        double radius = 3, height = 5; 
+        System.out.println("Volume: " + (Math.PI * radius * radius * height)); 
+    } 
+}
