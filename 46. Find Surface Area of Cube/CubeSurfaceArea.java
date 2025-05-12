c7f9933c205a0da8e929f19babbb4b444316eaf4
Gayanka <gayankadhrm@gmail.com>
2025-05-12 22:09:26 +0530

    Find surface area of cube

---
+++ 46. Find Surface Area of Cube/CubeSurfaceArea.java
@@ -0,0 +1,6 @@
+public class CubeSurfaceArea { 
+    public static void main(String[] args) { 
+        double side = 5; 
+        System.out.println("Surface Area: " + (6 * side * side)); 
+    } 
+}
