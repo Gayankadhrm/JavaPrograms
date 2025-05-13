e143195cc9f9f6c24e78add00d9e0e2f75184f0f
Gayanka <gayankadhrm@gmail.com>
2025-05-13 10:33:09 +0530

    Fid the surface area of cylinder

---
+++ 47. Find Surface Area of Cylinder/CylinderSurfaceArea.java
@@ -0,0 +1,7 @@
+public class CylinderSurfaceArea { 
+    public static void main(String[] args) { 
+        double radius = 3, height = 5; 
+        double surfaceArea = 2 * Math.PI * radius * (radius + height); 
+        System.out.println("Surface Area: " + surfaceArea); 
+    } 
+}
