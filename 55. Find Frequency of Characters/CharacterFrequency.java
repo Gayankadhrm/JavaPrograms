9d061415d3d0c54ab731acb64548f1cf4275db1d
Gayanka <gayankadhrm@gmail.com>
2025-06-05 20:57:38 +0530

    Find frequency of characters

---
+++ 55. Find Frequency of Characters/CharacterFrequency.java
@@ -0,0 +1,14 @@
+public class CharacterFrequency { 
+    public static void main(String[] args) { 
+        String str = "programming"; 
+        int[] freq = new int[256]; 
+        for(int i = 0; i < str.length(); i++) { 
+            freq[str.charAt(i)]++; 
+        } 
+        for(int i = 0; i < freq.length; i++) { 
+            if(freq[i] != 0) { 
+                System.out.println((char)i + ": " + freq[i]); 
+            } 
+        } 
+    } 
+}
